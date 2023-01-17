4d5fa6af0c7b6a4ae6b91a0432b897985106907f
Soto <ricasoto@deloitte.com>
2023-01-17 09:40:32 -0600

    Complete part2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,17 +3,32 @@ import Filter from './components/filter.component';
 import PersonForm from './components/person-form.component';
 import Persons from './components/persons.component';
 import personService from './services/persons';
+import './App.css';
+
+const ERROR = 'error';
+const SUCCESS = 'success';
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={message.className}>
+      {message.display}
+    </div>
+  )
+}
 
 const App = () => {
-  console.log('App entry point');
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filteredPersons, setFilteredPersons] = useState(persons);
   const [filter, setFilter] = useState('');
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    console.log('useEffect personService.getAll()');
     personService.getAll()
     .then(initialPersons => {
       setPersons(initialPersons)
@@ -22,13 +37,11 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log('useEffect setFilteredPersons. filter: ', filter);
     setFilteredPersons(persons.filter((person) => person.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())));
   }, [filter, persons])
 
 
   const onNameChange = (event) => {
-    console.log('onNameChange');
     setNewName(event.target.value);
   }
 
@@ -41,30 +54,32 @@ const App = () => {
   }
 
   const addPerson = (event) => {
-    console.log('addPerson');
     event.preventDefault();
     const person = persons.find(element => element.name === newName);
+    console.log(person);
     if(!person)
     {
       personService.create({ name: newName, number: newNumber })
       .then(personObject => {
-        setNewName('');
-        setNewNumber('');
         setPersons(persons.concat(personObject));
         setFilteredPersons(persons.concat(personObject));
+        setMessage({ display: `Added '${personObject.name}'`, className: SUCCESS });
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
       });
     }
     else if(window.confirm(`${newName} is already added to the phonebook, replace the old number with new one?`))
     {
       personService.update(person.id, { name: newName, number: newNumber })
-      .then((personObject) => {
-        setNewName('');
-        setNewNumber('');
-        personService.getAll()
-        .then(initialPersons => {
-          setPersons(initialPersons)
-          setFilteredPersons(persons);
-        });
+      .then((returnedPerson) => {
+        setPersons(persons.map(element => element.id !== returnedPerson.id ? element : returnedPerson));
+      }).catch(() => {
+        setMessage({ display: `Person '${person.name}' was already removed from server`, className: ERROR });
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+        setPersons(persons.filter(element => element.id !== person.id));
       })
     }
   }
@@ -83,6 +98,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <Filter  filter={filter} onFilterChange={onFilterChange} />
       <h2>add a new</h2>
       <PersonForm 
